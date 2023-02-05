4760ca3c0ddb7106859ef30ce3058dee4c005544
Stephane <sbornhauser@gmx.ch>
2023-02-05 11:26:25 +0100

    Added Test Project

---
+++ Test_DBA_SWE_PGA2/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Test_DBA_SWE_PGA2
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
